b3dc5769961ecf77fc5f1eb7b5280071f18ee7d6
KadMary <55906770+KadMary@users.noreply.github.com>
2020-12-13 13:00:00 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(function () {
+    // 1. Initialize fotorama manually.
+    var $fotoramaDiv = $('#fReview').fotorama();
+
+    // 2. Get the API object.
+    var fotorama = $fotoramaDiv.data('fotorama-review');
+});
